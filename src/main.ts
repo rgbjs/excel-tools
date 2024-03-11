8c844aeb0b91ab3da15ebabe7af816ae640b2349
feiYu <946686638@qq.com>
2024-03-11 17:44:53 +0800

    [update] 更新开发环境配置

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
 export { default as ExcelJS } from 'exceljs'
+// @ts-ignore
 export { default as ImportExcel } from './ImportExcel.js'
+// @ts-ignore
 export { default as exportExcel } from './exportExcel.js'
--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import { defineConfig } from 'vite'
 export default defineConfig({
 	build: {
 		lib: {
-			entry: './src/main.js',
-			name: 'EventBus',
+			entry: './src/main.ts',
+			name: 'excelTools',
 			formats: ['es', 'cjs', 'umd', 'iife'],
 			fileName(format, entryName) {
 				return `${entryName}.${format}.js`
