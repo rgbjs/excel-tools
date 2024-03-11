5e32f6ddb99333f12ea6f187fcd240c26ed0e983
feiYu <946686638@qq.com>
2024-03-11 22:38:48 +0800

    [update] 变更文件类型为ts

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,3 @@
 export { default as ExcelJS } from 'exceljs'
-// @ts-ignore
-export { default as ImportExcel } from './ImportExcel.js'
-// @ts-ignore
-export { default as exportExcel } from './exportExcel.js'
+export { default as ImportExcel } from './ImportExcel'
+export { default as exportExcel } from './exportExcel'
